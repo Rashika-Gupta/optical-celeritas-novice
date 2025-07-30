7b7ba2eb19a8bdccc84dd4589172ee246f37c7d9
Rashika-Gupta <rashika.gupta.ornl@gmail.com>
2025-07-30 09:56:11 -0400

    Optical error from celertias removed

---
+++ OpNovice/build-opnovice/CMakeFiles/OpNovice.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/r1i/Desktop/project/optical-celeritas-novice/OpNovice/OpNovice.cc" "CMakeFiles/OpNovice.dir/OpNovice.cc.o" "gcc" "CMakeFiles/OpNovice.dir/OpNovice.cc.o.d"
+  "/Users/r1i/Desktop/project/optical-celeritas-novice/OpNovice/src/MakeCelerOptions.cc" "CMakeFiles/OpNovice.dir/src/MakeCelerOptions.cc.o" "gcc" "CMakeFiles/OpNovice.dir/src/MakeCelerOptions.cc.o.d"
+  "/Users/r1i/Desktop/project/optical-celeritas-novice/OpNovice/src/OpNoviceActionInitialization.cc" "CMakeFiles/OpNovice.dir/src/OpNoviceActionInitialization.cc.o" "gcc" "CMakeFiles/OpNovice.dir/src/OpNoviceActionInitialization.cc.o.d"
+  "/Users/r1i/Desktop/project/optical-celeritas-novice/OpNovice/src/OpNoviceCeleritasTrackingAction.cc" "CMakeFiles/OpNovice.dir/src/OpNoviceCeleritasTrackingAction.cc.o" "gcc" "CMakeFiles/OpNovice.dir/src/OpNoviceCeleritasTrackingAction.cc.o.d"
+  "/Users/r1i/Desktop/project/optical-celeritas-novice/OpNovice/src/OpNoviceDetectorConstruction.cc" "CMakeFiles/OpNovice.dir/src/OpNoviceDetectorConstruction.cc.o" "gcc" "CMakeFiles/OpNovice.dir/src/OpNoviceDetectorConstruction.cc.o.d"
+  "/Users/r1i/Desktop/project/optical-celeritas-novice/OpNovice/src/OpNoviceDetectorMessenger.cc" "CMakeFiles/OpNovice.dir/src/OpNoviceDetectorMessenger.cc.o" "gcc" "CMakeFiles/OpNovice.dir/src/OpNoviceDetectorMessenger.cc.o.d"
+  "/Users/r1i/Desktop/project/optical-celeritas-novice/OpNovice/src/OpNoviceEventAction.cc" "CMakeFiles/OpNovice.dir/src/OpNoviceEventAction.cc.o" "gcc" "CMakeFiles/OpNovice.dir/src/OpNoviceEventAction.cc.o.d"
+  "/Users/r1i/Desktop/project/optical-celeritas-novice/OpNovice/src/OpNoviceGDMLDetectorConstruction.cc" "CMakeFiles/OpNovice.dir/src/OpNoviceGDMLDetectorConstruction.cc.o" "gcc" "CMakeFiles/OpNovice.dir/src/OpNoviceGDMLDetectorConstruction.cc.o.d"
+  "/Users/r1i/Desktop/project/optical-celeritas-novice/OpNovice/src/OpNovicePrimaryGeneratorAction.cc" "CMakeFiles/OpNovice.dir/src/OpNovicePrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/OpNovice.dir/src/OpNovicePrimaryGeneratorAction.cc.o.d"
+  "/Users/r1i/Desktop/project/optical-celeritas-novice/OpNovice/src/OpNovicePrimaryGeneratorMessenger.cc" "CMakeFiles/OpNovice.dir/src/OpNovicePrimaryGeneratorMessenger.cc.o" "gcc" "CMakeFiles/OpNovice.dir/src/OpNovicePrimaryGeneratorMessenger.cc.o.d"
+  "/Users/r1i/Desktop/project/optical-celeritas-novice/OpNovice/src/OpNoviceRun.cc" "CMakeFiles/OpNovice.dir/src/OpNoviceRun.cc.o" "gcc" "CMakeFiles/OpNovice.dir/src/OpNoviceRun.cc.o.d"
+  "/Users/r1i/Desktop/project/optical-celeritas-novice/OpNovice/src/OpNoviceRunAction.cc" "CMakeFiles/OpNovice.dir/src/OpNoviceRunAction.cc.o" "gcc" "CMakeFiles/OpNovice.dir/src/OpNoviceRunAction.cc.o.d"
+  "/Users/r1i/Desktop/project/optical-celeritas-novice/OpNovice/src/OpNoviceSensitiveDetector.cc" "CMakeFiles/OpNovice.dir/src/OpNoviceSensitiveDetector.cc.o" "gcc" "CMakeFiles/OpNovice.dir/src/OpNoviceSensitiveDetector.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ OpNovice/build-opnovice/CMakeFiles/OpNovice.dir/src/OpNoviceCeleritasTrackingAction.cc.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/OpNovice.dir/src/OpNoviceCeleritasTrackingAction.cc.o: \
+  /Users/r1i/Desktop/project/optical-celeritas-novice/OpNovice/src/OpNoviceCeleritasTrackingAction.cc
